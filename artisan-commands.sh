29a735a7e7a3c2352d62b01e4690b3fb1d5968af
flads <fabio.souza.rn@hotmail.com>
2020-01-29 10:12:20 -0300

    Adding the list of git and artisan commands.

---
+++ artisan-commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+php artisan make:model Project -mcr
+# --> php artisan make:model Project --migration --controller --resource
+
+php artisan make:controller UserController
+# --> php artisan make:controller UserController --resource
+# --> php artisan make:controller UserController --api
+
+php artisan make:migration create_projects_table
+
+php artisan make:command SendEmails --command=Command
