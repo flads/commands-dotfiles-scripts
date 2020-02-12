2beac4d3a08a7c177ffea52c88bb92a245f340cd
flads <fabio.souza.rn@hotmail.com>
2020-02-12 13:35:31 -0300

    Adding BFG command.

--- linux-commands.sh
+++ linux-commands.sh
@@ -1,2 +1,5 @@
 # This command will print the exact status of your system:
 ubuntu-support-status --show-all
+
+# BFG Repo-Cleaner
+java -jar bfg.jar  --replace-text replacements.txt -fi *.php  my-repo.git
