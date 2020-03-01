925c4cd8c5f24124e992540acf17391e50f169cf
flads <fabio.souza.rn@hotmail.com>
2020-03-01 05:07:08 -0300

    Insert the command to install the 'vim-plug' and add the 'composer' to the title for the software to be installed

--- install-development-software.sh
+++ install-development-software.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#---------------------------------------#
-# PHP, GIT, GIT-FLOW, VIM, WGET AND CURL:
-#---------------------------------------#
+#-----------------------------------------------------------#
+# PHP, GIT, GIT-FLOW, VIM, VIM-PLUG, WGET, CURL AND COMPOSER:
+#-----------------------------------------------------------#
 
 sudo apt-get update
 sudo apt-get upgrade -y
+
 sudo apt-get install software-properties-common -y
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:certbot/certbot -y
@@ -13,11 +14,19 @@ sudo apt-get update
 
 sudo apt-get install git git-flow vim curl wget -y
 
-sudo apt-get install php7.3 php7.3-bcmath php7.3-cgi php7.3-cli php7.3-curl php7.3-dev php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-intl php7.3-json php7.3-mbstring php7.3-mysql php7.3-opcache php7.3-pgsql php7.3-pspell php7.3-readline php7.3-soap php7.3-sqlite3 php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl php7.3-zip php-imagick -y
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sudo apt-get install php7.3 php7.3-bcmath php7.3-cgi php7.3-cli php7.3-curl php7.3-dev \ 
+    php7.3-enchant php7.3-fpm php7.3-gd php7.3-gmp php7.3-intl php7.3-json php7.3-mbstring \ 
+    php7.3-mysql php7.3-opcache php7.3-pgsql php7.3-pspell php7.3-readline php7.3-soap \ 
+    php7.3-sqlite3 php7.3-tidy php7.3-xml php7.3-xmlrpc php7.3-xsl php7.3-zip php-imagick -y
 
 sudo a2enconf php7.3-fpm
 
-sudo curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer && composer self-update
+sudo curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer 
+composer self-update
 
 # ----------------------- #
 #  NODEJS AND NPM WITH NVM:
